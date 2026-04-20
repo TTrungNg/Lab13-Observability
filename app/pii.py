9c506d3df2defe22a57d3b1418397537a25403b4
KhanhHaV <havietkhanh49@gmail.com>
2026-04-20 15:50:23 +0700

    feature: Logging

--- app/pii.py
+++ app/pii.py
@@ -8,7 +8,8 @@ PII_PATTERNS: dict[str, str] = {
     "phone_vn": r"(?:\+84|0)[ \.-]?\d{3}[ \.-]?\d{3}[ \.-]?\d{3,4}", # Matches 090 123 4567, 090.123.4567, etc.
     "cccd": r"\b\d{12}\b",
     "credit_card": r"\b\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}\b",
-    # TODO: Add more patterns (e.g., Passport, Vietnamese address keywords)
+    "passport": r"\b[A-Z]{1,2}\d{6,9}\b",
+    "vn_address": r"\b(?:Đường|Phố|Phường|Xã|Quận|Huyện|Thành phố|TP\.)\s+[\wÀ-ỹ\s\.,0-9/-]{3,80}",
 }
 
 
